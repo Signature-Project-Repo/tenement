a6ae307791ed7b1f77c30ca7b2259d57ce6681fc
Joel Shibu <joelshibu100@gmail.com>
2023-08-27 13:02:03 +0530

    Initial commit

---
+++ action.php
@@ -0,0 +1,18 @@
+<?php
+$fname=$_POST['fname'];
+echo "$fname<br>";
+$lname=$_POST['lname'];
+echo "$lname<br>";
+$district=$_POST['district'];
+echo "$district<br>";
+$dob=$_POST['dob'];
+echo "$dob<br>";
+$gender=$_POST['gender'];
+echo "$gender<br>";
+$email=$_POST['email'];
+echo "$email<br>";
+$number=$_POST['number'];
+echo "$number<br>";
+$password=$_POST['password'];
+echo "$password<br>";
+?>
