b5023407b42ef5d5fa8e157f00f7cc1ab93afa04
Joel Shibu <joelshibu100@gmail.com>
2023-09-16 13:08:40 +0530

    commit22

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,4 @@
-
-<?php 
+<?php
 
 include 'header.php';
 
@@ -7,23 +6,104 @@ include 'header.php';
 
 
 
-  <main id="main" class="main">
+<main id="main" class="main">
 
-    <div class="pagetitle">
-      <h1>Dashboard</h1>
-      <nav>
-        <ol class="breadcrumb">
-          <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-          <li class="breadcrumb-item active">Dashboard</li>
-        </ol>
-      </nav>
-    </div><!-- End Page Title -->
+  <div class="pagetitle">
+    <h1>Manage Items</h1>
+    <nav>
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+        <li class="breadcrumb-item active">Items</li>
+      </ol>
+    </nav>
 
-   
-  </main><!-- End #main -->
+  </div><!-- End Page Title -->
+
+  <section class="section">
+    <div class="row">
 
 
-<?php 
+
+      <div class=col-md-6>
+
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Labels Form</h5>
+
+
+            <form class="row g-3">
+              <div class="col-md-12">
+                <div class="form-floating">
+                  <input type="text" class="form-control" id="floatingName" placeholder="Your Name">
+                  <label for="floatingName">Services</label>
+                </div>
+              </div>
+
+              <div class="col-12">
+                <div class="form-floating">
+                  <textarea class="form-control" placeholder="Address" id="floatingTextarea" style="height: 100px;"></textarea>
+                  <label for="floatingTextarea">Description</label>
+                </div>
+              </div>
+
+              <div class="text-center">
+                <button type="submit" class="btn btn-primary">Add</button>
+
+              </div>
+            </form><!-- End floating Labels Form -->
+
+          </div>
+        </div>
+
+      </div>
+
+      <div class="col-lg-6">
+
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title"></h5>
+            
+
+            <!-- Table with stripped rows -->
+            <table class="table datatable">
+              <thead>
+                <tr>
+                  <th scope="col">Sl No.</th>
+              
+                  <th scope="col">Service</th>
+                  <th scope="col">Description</th>
+
+                  
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">1</th>
+                  <td>Brandon Jacob</td>
+                  <td>Designer</td>
+                  <td>28</td>
+                  <td>2016-05-25</td>
+                </tr>
+          
+              </tbody>
+            </table>
+            <!-- End Table with stripped rows -->
+
+          </div>
+        </div>
+
+      </div>
+
+
+    </div>
+  </section>
+  
+
+
+</main><!-- End #main -->
+
+
+<?php
 
 include 'footer.html';
 
