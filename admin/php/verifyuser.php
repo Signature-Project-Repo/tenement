89cb4270d44735f26fdfad3d54f6f122763886c4
Joel Shibu <joelshibu100@gmail.com>
2023-09-16 11:17:39 +0530

    commit21

--- admin/php/verifyuser.php
+++ admin/php/verifyuser.php
@@ -1,95 +1,90 @@
 <html>
-    <head>
-    <script type="text/javascript" src="swal/jquery.min.js"></script>
-    <script type="text/javascript" src="swal/bootstrap.min.js"></script>
-    <script type="text/javascript" src="swal/sweetalert2@11.js"></script>
+
+<head>
+  <script type="text/javascript" src="swal/jquery.min.js"></script>
+  <script type="text/javascript" src="swal/bootstrap.min.js"></script>
+  <script type="text/javascript" src="swal/sweetalert2@11.js"></script>
 </head>
 </body>
 
 
 
-<?php 
+<?php
 require '../../connect.php';
 
 
 
-$email=$_GET['email'];
-$status=$_GET['status'];
+$email = $_GET['email'];
+$status = $_GET['status'];
 // echo $email." ".$status;
-$sql="UPDATE loginpage SET status='$status' WHERE email='$email'";
-$result=update_data($sql);
+$sql = "UPDATE loginpage SET status='$status' WHERE email='$email'";
+$result = update_data($sql);
 
-if(update_data($sql)&&($status==1))
-{
+if (update_data($sql) && ($status == 1)) {
 ?>
-<script>
- 
- let timerInterval
-Swal.fire({
-  icon:'success',
-  title: 'User Accepted',
-  html: 'updating ',
-  timer: 1300,
-  timerProgressBar: true,
-  didOpen: () => {
-    Swal.showLoading()
-    const b = Swal.getHtmlContainer().querySelector('b')
-    timerInterval = setInterval(() => {
-      b.textContent = Swal.getTimerLeft()
-    }, 100)
-  },
-  willClose: () => {
-    clearInterval(timerInterval)
-  }
-}).then((result) => {
-  /* Read more about handling dismissals below */
-  if (result.dismiss === Swal.DismissReason.timer) {
-    window.location.replace('../pendinguser.php');
+  <script>
+    let timerInterval
+    Swal.fire({
+      icon: 'success',
+      title: 'User Accepted',
+      html: 'updating ',
+      timer: 1300,
+      timerProgressBar: true,
+      didOpen: () => {
+        Swal.showLoading()
+        const b = Swal.getHtmlContainer().querySelector('b')
+        timerInterval = setInterval(() => {
+          b.textContent = Swal.getTimerLeft()
+        }, 100)
+      },
+      willClose: () => {
+        clearInterval(timerInterval)
+      }
+    }).then((result) => {
+      /* Read more about handling dismissals below */
+      if (result.dismiss === Swal.DismissReason.timer) {
+        window.location.replace('../pendinguser.php');
 
-  }
-});
-</script>
+      }
+    });
+  </script>
 <?php
 
 }
 
-if(update_data($sql)&&($status==-1))
-{
+if (update_data($sql) && ($status == -1)) {
 
 ?>
 
-<script>
- 
- let timerInterval
-Swal.fire({
-  icon:'error',
-  title: 'User Rejected',
-  html: 'updating ',
-  timer: 1300,
-  timerProgressBar: true,
-  didOpen: () => {
-    Swal.showLoading()
-    const b = Swal.getHtmlContainer().querySelector('b')
-    timerInterval = setInterval(() => {
-      b.textContent = Swal.getTimerLeft()
-    }, 100)
-  },
-  willClose: () => {
-    clearInterval(timerInterval)
-  }
-}).then((result) => {
-  /* Read more about handling dismissals below */
-  if (result.dismiss === Swal.DismissReason.timer) {
-    window.location.replace('../pendinguser.php');
+  <script>
+    let timerInterval
+    Swal.fire({
+      icon: 'error',
+      title: 'User Rejected',
+      html: 'updating ',
+      timer: 1300,
+      timerProgressBar: true,
+      didOpen: () => {
+        Swal.showLoading()
+        const b = Swal.getHtmlContainer().querySelector('b')
+        timerInterval = setInterval(() => {
+          b.textContent = Swal.getTimerLeft()
+        }, 100)
+      },
+      willClose: () => {
+        clearInterval(timerInterval)
+      }
+    }).then((result) => {
+      /* Read more about handling dismissals below */
+      if (result.dismiss === Swal.DismissReason.timer) {
+        window.location.replace('../pendinguser.php');
 
-  }
-});
-</script>
+      }
+    });
+  </script>
 <?php
 }
 ?>
-    </body>
+</body>
+
 </html
-
-
-
